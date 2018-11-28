db75f0225c9d64725c9b59ead1f5142dac5e1179
maccybee <mcadam88@gmail.com>
2018-11-28 17:06:55 +0000

    folder restructure

--- src/test/scala/BulkScan/Login/BulkScan.scala
+++ src/test/scala/BulkScan/Login/BulkScan.scala
@@ -1,33 +1,21 @@
 package BulkScan.Login
 
 import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import io.gatling.core.scenario.Simulation
 
 class BulkScan extends Simulation {
 
 	//Script variables defined below
-
 	val localfilepath = "/Users/jonathanmcadam/simple-gatling-tests-framework/bulk-scan-performance-tests/src/test/resources/data/zip_files/"
 	val feeder = csv("/Users/jonathanmcadam/simple-gatling-tests-framework/bulk-scan-performance-tests/src/test/resources/data/zip_files/MyData.csv").queue
 	val header_01 = Map("Ocp-Apim-Subscription-Key" -> "b78fb11dad304396982bb647ff4d979b")
 	val header_02 = Map("x-ms-blob-type" -> "BlockBlob", "Content-Type" -> "application/zip")
-	val httpConf = http
-		.baseURL("https://core-api-mgmt-sprod.azure-api.net")
+	val httpConf = http.baseURL("https://core-api-mgmt-sprod.azure-api.net")
 	val url2 = "https://bulkscan.sprod.platform.hmcts.net"
 
-	//object GetSasToken {
-
-		//val header_01 = Map("Ocp-Apim-Subscription-Key" -> "b78fb11dad304396982bb647ff4d979b")
-		//val httpConf = http.baseURL("https://core-api-mgmt-sprod.azure-api.net")
-		//val url2 = "https://bulkscansprod.blob.core.windows.net"
-		//val zipfilename = "1_24-06-2018-00-00-50.zip"
-		//val localfilepath = "/Users/jonathanmcadam/simple-gatling-tests-framework/src/test/resources/data/"
-
 		object GetSasToken {
-
-			//val header_01 = Map("Ocp-Apim-Subscription-Key" -> "0bd0296e813e428d9e2a24ad6ef3b2c1")
 
 			val gettoken = exec(http("001_GetToken")
 				.get("/bulk-scan/token/sscs")
@@ -39,13 +27,9 @@ class BulkScan extends Simulation {
 
 		object SendFile {
 
-			val FileUpload = //scenario("Bulk Scan").repeat(10) {
-			//exec(_.set("fileName", zipfilename))
-				exec(_.set("filepath", localfilepath))
+			val FileUpload = exec(_.set("filepath", localfilepath))
 					.feed(feeder)
 					.exec(http("002_FileUpload")
-						//.put(url2 + "/sscs/${zipfile}?${SaS_Token}")
-						//.put(url2 + "/sscs/${zipfile}?sig=2%2FBp1DLeN8u%2BVAgjWMojnFpHTywOF6aPBfNU%2FsFi96A%3D&se=2018-10-31T10%3A01%3A36Z&sv=2018-03-28&sp=wl&sr=c")
 						.put(url2 + "/sscs/${zipfile}?${SaS_Token}")
 						.proxy(Proxy("proxyout.reform.hmcts.net", 8080))
 						.headers(header_02)
---
+++ target/gatling/bulkscan-1543424787338/js/stats.js
@@ -0,0 +1,281 @@
+var stats = {
+    type: "GROUP",
+name: "Global Information",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "Global Information",
+    "numberOfRequests": {
+        "total": "2",
+        "ok": "2",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "1033",
+        "ok": "1033",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "718",
+        "ok": "718",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "1033",
+        "ok": "1033",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "1391",
+        "ok": "1391",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "1678",
+        "ok": "1678",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1736",
+        "ok": "1736",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 50
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 1,
+        "percentage": 50
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.667",
+        "ok": "0.667",
+        "ko": "-"
+    }
+},
+contents: {
+"req_001-gettoken-86e8c": {
+        type: "REQUEST",
+        name: "001_GetToken",
+path: "001_GetToken",
+pathFormatted: "req_001-gettoken-86e8c",
+stats: {
+    "name": "001_GetToken",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "315",
+        "ok": "315",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.333",
+        "ok": "0.333",
+        "ko": "-"
+    }
+}
+    },"req_002-fileupload-53d76": {
+        type: "REQUEST",
+        name: "002_FileUpload",
+path: "002_FileUpload",
+pathFormatted: "req_002-fileupload-53d76",
+stats: {
+    "name": "002_FileUpload",
+    "numberOfRequests": {
+        "total": "1",
+        "ok": "1",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "1750",
+        "ok": "1750",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 1,
+        "percentage": 100
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.333",
+        "ok": "0.333",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
