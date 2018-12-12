76d77d03cd6ae44479bb8b5bd899353c6fa1b026
bugbare-hmcts <rishi.shah@hmcts.net>
2018-12-12 13:05:54 +0000

    Added package management for sensitive data, update gitignore and removed orphaned DS_Store files

--- src/test/scala/sensitiveData.scala
+++
@@ -1,9 +0,0 @@
-import io.gatling.core.Predef.csv
-
-//Script variables defined below
-
-val ccdfilepath = "/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/ccdCases/"
-val sscsfilepath = "/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/sscsCases/"
-val ccdfeeder = csv("/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/ccdCases/ccdData.csv")
-val sscsfeeder = csv("/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/sscsCases/sscsData.csv")
-val header_01 = Map("Ocp-Apim-Subscription-Key" -> "b78fb11dad304396982bb647ff4d979b")
