dd4a7b8a91017245550e3dd393f7704b5775a0a8
maccybee <mcadam88@gmail.com>
2018-12-18 15:43:04 +0000

    Update sensitiveData.scala

--- src/test/scala/sensitiveData.scala
+++ src/test/scala/sensitiveData.scala
@@ -6,4 +6,4 @@ val ccdfilepath = "/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/ccdCa
 val sscsfilepath = "/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/sscsCases/"
 val ccdfeeder = csv("/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/ccdCases/ccdData.csv")
 val sscsfeeder = csv("/Users/jonathanmcadam/Documents/Work/MOJ/dataForTesting/sscsCases/sscsData.csv")
-val header_01 = Map("Ocp-Apim-Subscription-Key" -> "b78fb11dad304396982bb647ff4d979b")
+val header_01 = Map("Ocp-Apim-Subscription-Key" -> "expired-key")
